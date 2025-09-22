efeb242788bbeaafbe1f32b1b4a471179c912325
DalenGordon <dgordo34@charlotte.edu>
2025-09-22 16:39:41 -0400

    Adding Dev Test

---
+++ dev-test.js
@@ -0,0 +1,5 @@
+const datePicker = new DatePicker("div1", function (id, fixedDate) {
+    console.log("DatePicker with id", id,
+        "selected date:", fixedDate.month + "/" + fixedDate.day + "/" + fixedDate.year);
+});
+datePicker.render(new Date("July 4, 1776"));
